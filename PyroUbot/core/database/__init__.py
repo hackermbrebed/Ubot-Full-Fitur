d488c194f7a52038ca163b1a7d968d3ef8882fdf
hackermbrebed <48652196+hackermbrebed@users.noreply.github.com>
2025-11-04 20:39:42 +0700

    updt

--- PyroUbot/core/database/__init__.py
+++ PyroUbot/core/database/__init__.py
@@ -5,6 +5,7 @@ from PyroUbot.config import MONGO_URL
 mongo_client = AsyncIOMotorClient(MONGO_URL)
 mongodb = mongo_client.userbotxx
 
+from .two_factor import set_two_factor, get_two_factor
 from PyroUbot.core.database.expired import *
 from PyroUbot.core.database.userbot import *
 from PyroUbot.core.database.pref import *
